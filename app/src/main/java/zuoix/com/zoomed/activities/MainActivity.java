3fc461a9c27f36472602f22729c5e2e19effa199
fritz <gnhsglenn@gmail.com>
2019-05-28 15:15:39 +0100

    comand fragment update

--- app/src/main/java/zuoix/com/zoomed/activities/MainActivity.java
+++ app/src/main/java/zuoix/com/zoomed/activities/MainActivity.java
@@ -1,17 +1,31 @@
 package zuoix.com.zoomed.activities;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
-
+import android.support.v7.widget.Toolbar;
 import zuoix.com.zoomed.R;
+import zuoix.com.zoomed.fragment.AccountFragment;
+import zuoix.com.zoomed.fragment.CommandFragment;
+import zuoix.com.zoomed.fragment.SettingFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    final Fragment account = new AccountFragment();
+    final Fragment setting = new SettingFragment();
+    final Fragment command = new CommandFragment();
+
+    Toolbar toolbar;
+    BottomNavigationView navigation;
+
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = command;
+    Context context;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,14 +33,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.command:
+                    fm.beginTransaction().hide(active).show(command).commit();
+                    active = command;
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+
+                case R.id.account:
+                    fm.beginTransaction().hide(active).show(account).commit();
+                    active = account;
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+
+                case R.id.setting:
+                    fm.beginTransaction().hide(active).show(setting).commit();
+                    active = setting;
                     return true;
             }
             return false;
@@ -37,10 +56,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
+        fm.beginTransaction().add(R.id.main_container, account, "3").hide(account).commit();
+        fm.beginTransaction().add(R.id.main_container, setting, "2").hide(setting).commit();
+        fm.beginTransaction().add(R.id.main_container,command, "1").commit();
+        navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        context = this;
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
+    @Override
+    public void onBackPressed() {
+        if(active != command){
+            navigation.setSelectedItemId(R.id.command);
+            active = command;
+        }else{
+            finish();
+        }
+    }
 }
--- app/src/main/java/zuoix/com/zoomed/fragment/CommandFragment.java
+++ app/src/main/java/zuoix/com/zoomed/fragment/CommandFragment.java
@@ -2,18 +2,27 @@ package zuoix.com.zoomed.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import zuoix.com.zoomed.AutoFitGridRecyclerView;
+import zuoix.com.zoomed.BaseApplication;
+import zuoix.com.zoomed.CommandAdapter;
 import zuoix.com.zoomed.R;
+import zuoix.com.zoomed.activities.SharedPref;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CommandFragment extends Fragment {
+    AutoFitGridRecyclerView commandView;
+    RecyclerView.Adapter adapter;
 
 
     public CommandFragment() {
@@ -26,6 +35,17 @@ public class CommandFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_command, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        commandView = view.findViewById(R.id.command_list);
+        adapter = new CommandAdapter(BaseApplication.getInstance()
+                .getCommandList(new SharedPref(getContext()).getGeneration()),getContext());
+        commandView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 
 }
